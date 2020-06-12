9f26bcda226032506c26edfc2c84ce3e480c4237
Virginia Davenport <virginialdport@gmail.com>
2020-06-12 09:23:18 -0600

    getting response from api

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,30 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
+import axios from 'axios'
+
+function App() {
+  const [data, setData] = useState([])
+  const [smurfInput, setSmurfInput]= useState({})
+  const [refresh, setRefresh] = useState(true)
+
+  useEffect(() => {
+    axios.get("http://localhost:3333/smurfs")
+      .then(res => {
+        console.log(res)
+      })
+  },[])
+  
+  
+  
+  return (
+    <div className="App">
+      <h1>SMURFS! 2.0 W/ Redux</h1>
+      <div>Welcome to your state management version of Smurfs!</div>
+      <div>Start inside of your `src/index.js` file!</div>
+      <div>Have fun!</div>
+    </div>
+  );
+  
 }
 
 export default App;
